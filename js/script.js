203c582668dcacd3b51f520ece98a39b3294fd28
EdgarAlvarez-ROl <edgaralvares4208@gmail.com>
2024-01-08 14:18:16 -0600

    config  icon  and js 11 option

--- js/script.js
+++ js/script.js
@@ -181,11 +181,16 @@ function stopRotateWheel() {
   ctx.save();
   ctx.font = 'bold 70px Helvetica, Arial';
   var text = options[index]
+  var lineHeight = 0; // Puedes ajustar este valor según sea necesario
   if (text == '11') {
+    text += "\nChupalo Entonces";
     ctx.font = 'bold 22px Helvetica, Arial';
-    text += " chupalo entonces";
+    var lineHeight = 30;
+  }// Muestra cada línea de texto por separado
+  var lines = text.split('\n');
+  for (var i = 0; i < lines.length; i++) {
+    ctx.fillText(lines[i], 250 - ctx.measureText(lines[i]).width / 2, 250 + 10 + i * lineHeight);
   }
-  ctx.fillText(text, 250 - ctx.measureText(text).width / 2, 250 + 10);
   ctx.restore();
   
 }
