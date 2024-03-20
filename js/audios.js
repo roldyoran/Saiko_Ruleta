1681d0d5a19e6f02ed4f5c6de4af4f4f5d746563
EdgarAlvarez-ROl <edgaralvares4208@gmail.com>
2024-03-20 12:10:09 -0600

    test audios music random

---
+++ js/audios.js
@@ -0,0 +1,23 @@
+function playVideo() {
+  const playButton = document.getElementById("playButton");
+//   const audioFiles = ["bad.mp3", "inspiracion.mp3",  "soygay.mp3", "depilate.mp3", "mujeres.mp3"];
+  const audioFiles = ["depilate.mp3"];  
+  const randomIndex = Math.floor(Math.random() * audioFiles.length);
+  const audioSrc = "public/" + audioFiles[randomIndex];
+  var audio = document.getElementById("cancion");
+
+  if (audio.paused) {
+    audio.src = audioSrc;
+    audio.play();
+    playButton.classList.add("text-green-600");
+  } else {
+    audio.pause();
+    playButton.classList.remove("text-green-600");
+    playButton.classList.add("text-black");
+  }
+
+  audio.addEventListener("ended", function () {
+    playButton.classList.remove("text-green-600");
+    playButton.classList.add("text-black");
+  });
+}
