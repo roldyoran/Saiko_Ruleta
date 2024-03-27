cee5c548758c8ebd07102411264b35a0ca95e985
EdgarAlvarez-ROl <edgaralvares4208@gmail.com>
2024-03-27 17:48:52 -0600

    add dj script js in file

---
+++ js/dj_caminando.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var container = document.getElementById("animatedImageContainer");
+
+  // Agrega la clase "animate-in" para mostrar la imagen
+  container.classList.add("animate-in");
+
+  setTimeout(function () {
+    // Agrega la clase "animate-out" para ocultar la imagen
+    container.classList.add("animate-out");
+
+    setTimeout(function () {
+      var image = document.getElementById("animatedImage");
+      image.src = "public/DJ2.gif";
+    }, 10);
+
+    setTimeout(function () {
+      // Elimina el contenedor después de la animación de salida
+      container.parentNode.removeChild(container);
+    }, 8000);
+  }, 8000); // Retraso para aplicar la transformación de espejo
+});
