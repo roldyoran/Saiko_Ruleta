d336ae6c9b53f003726ffb486fd5530b0d31b0d4
EdgarAlvarez-ROl <edgaralvares4208@gmail.com>
2024-09-24 23:58:47 -0600

    add workflows to make astro in github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,5 +8,7 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://roldyoran.github.io',
+  base: 'Saiko_Ruleta',
   integrations: [tailwind(), react(), mdx()]
 });
