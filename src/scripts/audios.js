88a83e33fb3eadd9c533995b94e8f0b61f1243ae
EdgarAlvarez-ROl <edgaralvares4208@gmail.com>
2024-10-22 01:12:08 -0600

    ADD audio hadesu op random2

--- src/scripts/audios.js
+++ src/scripts/audios.js
@@ -2,11 +2,11 @@
 document.getElementById("playMusic").addEventListener("click", playMusic);
 function playMusic() {
   const playButton = document.getElementById("playButton");
-  const audioFiles = ["anime_fin.mp3", "trap_fin.mp3", "yugi_op.mp3", "saiko_qlao.mp3"]; // "anime_fin.mp3", "trap_fin.mp3", "rock_fin.mp3", 
+  const audioFiles = ["anime_fin.mp3", "trap_fin.mp3", "yugi_op.mp3", "saiko_qlao.mp3", "op_random2.mp3"]; // "anime_fin.mp3", "trap_fin.mp3", "rock_fin.mp3", 
   const randomIndex = Math.floor(Math.random() * audioFiles.length);
   const audioSrc = "audios/" + audioFiles[randomIndex];
   var audio = document.getElementById("cancion");
-  // ajustarVolumen(0.9);
+  ajustarVolumen(0.7);
 
   if (audio.paused) {
     audio.src = audioSrc;
@@ -21,8 +21,8 @@ function playMusic() {
     playButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" id="playButton" class="icon icon-tabler icons-tabler-outline icon-tabler-player-play"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M7 4v16l13 -8z" /></svg>';
   });
 
-  // function ajustarVolumen(nuevoVolumen) {
-  //   nuevoVolumen = Math.max(0, Math.min(1, nuevoVolumen));
-  //   audio.volume = nuevoVolumen;
-  // }
+  function ajustarVolumen(nuevoVolumen) {
+    nuevoVolumen = Math.max(0, Math.min(1, nuevoVolumen));
+    audio.volume = nuevoVolumen;
+  }
 }
