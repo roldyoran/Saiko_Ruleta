8411d3415cbda4058179a2180c76ecb3e9e47b52
EdgarAlvarez-ROl <edgaralvares4208@gmail.com>
2024-11-28 22:22:37 -0600

    add: animation to title

--- src/scripts/animations.js
+++ src/scripts/animations.js
@@ -1,4 +1,14 @@
-import { animate, stagger, delay } from "motion";
+import { animate, stagger, delay, spring } from "motion";
+
+
+const title = document.querySelector("#title");
+
+animate(title,
+  { opacity: [0, 1], y: [-100, 20], easing: spring()},
+  { delay: 1 }
+);
+
+
 
 const images = document.querySelectorAll(".carousel .img");
 
@@ -30,23 +40,25 @@ setInterval(startAnimation, 21000); // 3000ms = 3 segundos (ajustar según la du
 const images2 = document.querySelectorAll(".carousel2 .img2");
 // Función comenzar la animación
 function startAnimation2() {
-    delay(() => {
-      // Aplica la animación de movimiento
-      animate(
-        images2,
-        { opacity: [0, 1] },
-        { duration: 1, delay: stagger(5) }
-      );
-    }, 5);
-    // Aplica la animación de desaparición para la ultima imagen
-    animate(images2,
-      { opacity: [1, 0], duration: 1 }
+  delay(() => {
+    // Aplica la animación de movimiento
+    animate(
+      images2,
+      { opacity: [0, 1] },
+      { duration: 1, delay: stagger(5) }
     );
-  }
-  
-  // Iniciar la animación al principio
-  startAnimation2();
+  }, 5);
+  // Aplica la animación de desaparición para la ultima imagen
+  animate(images2,
+    { opacity: [1, 0], duration: 1 }
+  );
+}
+
+// Iniciar la animación al principio
+startAnimation2();
 
 
 // Repetir la animación después de que termine (según la duración total)
 setInterval(startAnimation2, 21000); // 3000ms = 3 segundos (ajustar según la duración total)
+
+
